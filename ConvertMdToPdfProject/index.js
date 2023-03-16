7092e48ed2b1d3eac4f085c97ac9804b0fae3155
Rivka Berlinski <RivkaB@skyvar.co.il>
2023-03-16 14:35:04 +0200

    Check the integrity of the file

--- ConvertMdToPdfProject/index.js
+++ ConvertMdToPdfProject/index.js
@@ -1,8 +1,26 @@
 const markdownpdf = require('markdown-pdf');
 const fs = require('fs');
 
-const markdown = fs.readFileSync('input.md', 'utf8');
+const filename = process.argv[2];
+
+if(!filename){
+  console.error('Error: You must provide a filename as an argument');
+  process.exit(1);
+}
+
+if(!fs.existsSync(filename)){
+  console.error(`Error: File '${filename}' does not exist`);
+  process.exit(1);
+}
+
+if(Path.extname(filename) !== '.md'){
+  console.error(`Error: File '${filename}' is not a Markdown file`);
+  process.exit(1);
+}
+
+const markdown = fs.readFileSync(filename, 'utf8');
 
 markdownpdf().from.string(markdown).to('output.pdf', function () {
   console.log('PDF created');
 });
+
