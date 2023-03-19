58949c04042cb045a9da61b7edb1af9ed707cfd5
Rivka Berlinski <RivkaB@skyvar.co.il>
2023-03-19 11:53:30 +0200

    Add raising an exception

--- ConvertMdToPdfProject/index.js
+++ ConvertMdToPdfProject/index.js
@@ -4,18 +4,15 @@ const fs = require('fs');
 const filename = process.argv[2];
 
 if(!filename){
-  console.error('Error: You must provide a filename as an argument');
-  process.exit(1);
+  throw new MissingFilename("Error: You must provide a filename as an argument");
 }
 
 if(!fs.existsSync(filename)){
-  console.error(`Error: File '${filename}' does not exist`);
-  process.exit(1);
+  throw new ExistFilename("Error: File '${filename}' does not exist");
 }
 
 if(Path.extname(filename) !== '.md'){
-  console.error(`Error: File '${filename}' is not a Markdown file`);
-  process.exit(1);
+  throw new FileTypeError("Error: File '${filename}' is not a Markdown file");
 }
 
 const markdown = fs.readFileSync(filename, 'utf8');
@@ -24,3 +21,22 @@ markdownpdf().from.string(markdown).to('output.pdf', function () {
   console.log('PDF created');
 });
 
+
+class MissingFilename extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "MissingFilename";
+  }
+  }
+class ExistFilename extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ExistFilename";
+  }
+  }
+class FileTypeError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "FileTypeError";
+  }
+  }
